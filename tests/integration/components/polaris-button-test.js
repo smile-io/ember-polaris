e2f3fda8edf1566e8c77956794d735982d27edf8
Andy Pye <apye@sweettoothhq.com>
2017-05-30 11:48:57 +0300

    Finish adding link functionality

--- tests/integration/components/polaris-button-test.js
+++ tests/integration/components/polaris-button-test.js
@@ -8,18 +8,56 @@ moduleForComponent('polaris-button', 'Integration | Component | polaris button',
 test('renders the correct HTML', function(assert) {
   // Basic button, using children attribute.
   this.render(hbs`{{polaris-button children="Look at my children"}}`);
-  let buttonText = this.$('button.Polaris-Button > span.Polaris-Button__Content > span').text().trim();
+  let $button = this.$(' > button.Polaris-Button');
+
+  let buttonText = $button.find(' > span.Polaris-Button__Content > span').text().trim();
   assert.equal(buttonText, 'Look at my children', 'basic button with children attribute - text');
+
 
   // Basic button, block form.
   this.render(hbs`{{#polaris-button}}Does this look blocky to you?{{/polaris-button}}`);
-  buttonText = this.$('button.Polaris-Button > span.Polaris-Button__Content > span').text().trim();
+  $button = this.$(' > button.Polaris-Button');
+
+  buttonText = $button.find(' > span.Polaris-Button__Content > span').text().trim();
   assert.equal(buttonText, 'Does this look blocky to you?', 'basic button in block form - text');
+
 
   // With a URL.
   this.render(hbs`{{#polaris-button url="http://www.somewhere.com/lets-go/"}}Links zwei drei vier{{/polaris-button}}`);
-  buttonText = this.$('a.Polaris-Button > span.Polaris-Button__Content > span').text().trim();
+  $button = this.$(' > a.Polaris-Button');
+
+  buttonText = $button.find(' > span.Polaris-Button__Content > span').text().trim();
   assert.equal(buttonText, 'Links zwei drei vier', 'link button - text');
-  let linkUrl = this.$('a.Polaris-Button').attr('href');
-  assert.equal(linkUrl, 'http://www.somewhere.com/lets-go/', 'link button - href');
+
+  let linkHref = $button.attr('href');
+  assert.equal(linkHref, 'http://www.somewhere.com/lets-go/', 'link button - href');
+
+  let dataPolarisUnstyled = $button.attr('data-polaris-unstyled');
+  assert.equal(dataPolarisUnstyled, 'true', 'link button - data-polaris-unstyled');
+
+  let linkTarget = $button.attr('target');
+  assert.equal(linkTarget, undefined, 'link button - target');
+
+  let linkRel = $button.attr('rel');
+  assert.equal(linkRel, undefined, 'link button - rel');
+
+
+  // With a URL and external flag set.
+  this.render(hbs`{{#polaris-button url="http://www.somewhere.com/lets-go/" external=true}}Links zwei drei vier{{/polaris-button}}`);
+  $button = this.$(' > a.Polaris-Button');
+
+  buttonText = $button.find(' > span.Polaris-Button__Content > span').text().trim();
+  assert.equal(buttonText, 'Links zwei drei vier', 'external link button - text');
+
+  linkHref = $button.attr('href');
+  assert.equal(linkHref, 'http://www.somewhere.com/lets-go/', 'external link button - href');
+
+  dataPolarisUnstyled = $button.attr('data-polaris-unstyled');
+  assert.equal(dataPolarisUnstyled, 'true', 'external link button - data-polaris-unstyled');
+
+  linkTarget = $button.attr('target');
+  assert.equal(linkTarget, '_blank', 'external link button - target');
+
+  linkRel = $button.attr('rel');
+  assert.equal(linkRel, 'noopener noreferrer', 'external link button - rel');
 });
