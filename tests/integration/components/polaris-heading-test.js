008284dd9932a0bb706bd6d284c4c117db8295ca
Andy Pye <apye@sweettoothhq.com>
2017-06-08 19:01:29 +0300

    Update tests to use ember-native-dom-helpers

--- tests/integration/components/polaris-heading-test.js
+++ tests/integration/components/polaris-heading-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { find, findAll } from 'ember-native-dom-helpers';
 
 moduleForComponent('polaris-heading', 'Integration | Component | polaris heading', {
   integration: true
@@ -8,9 +9,10 @@ moduleForComponent('polaris-heading', 'Integration | Component | polaris heading
 test('it renders the correct HTML', function(assert) {
   // Inline form with defaults.
   this.render(hbs`{{polaris-heading children="This is a heading"}}`);
-  let $headings = this.$(' > h2.Polaris-Heading');
-  assert.equal($headings.length, 1, 'inline with defaults - renders one h2 heading');
-  assert.equal($headings.text().trim(), 'This is a heading', 'inline with defaults - renders correct text');
+
+  let headingSelector = 'h2.Polaris-Heading';
+  assert.equal(findAll(headingSelector).length, 1, 'inline with defaults - renders one h2 heading');
+  assert.equal(find(headingSelector).innerText, 'This is a heading', 'inline with defaults - renders correct text');
 
   // Block form with element specified.
   this.render(hbs`
@@ -18,7 +20,8 @@ test('it renders the correct HTML', function(assert) {
       This is an emphasised heading
     {{/polaris-heading}}
   `);
-  $headings = this.$(' > em.Polaris-Heading');
-  assert.equal($headings.length, 1, 'block with customisation - renders one emphasised heading');
-  assert.equal($headings.text().trim(), 'This is an emphasised heading', 'block with customisation - renders correct text');
+
+  headingSelector = 'em.Polaris-Heading';
+  assert.equal(findAll(headingSelector).length, 1, 'block with customisation - renders one emphasised heading');
+  assert.equal(find(headingSelector).innerText, 'This is an emphasised heading', 'block with customisation - renders correct text');
 });
--- tests/integration/components/polaris-subheading-test.js
+++ tests/integration/components/polaris-subheading-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { findAll } from 'ember-native-dom-helpers';
 
 moduleForComponent('polaris-subheading', 'Integration | Component | polaris subheading', {
   integration: true
@@ -8,10 +9,13 @@ moduleForComponent('polaris-subheading', 'Integration | Component | polaris subh
 test('it renders the correct HTML', function(assert) {
   // Inline form with defaults.
   this.render(hbs`{{polaris-subheading children="This is a subheading"}}`);
-  let $subheadings = this.$(' > h3.Polaris-Subheading');
-  assert.equal($subheadings.length, 1, 'inline with defaults - renders one h3 subheading');
-  assert.equal($subheadings.text().trim(), 'This is a subheading', 'inline with defaults - renders correct text');
-  assert.equal($subheadings.attr('aria-label'), 'This is a subheading', 'inline with defaults - adds correct label');
+
+  let subheadings = findAll('h3.Polaris-Subheading');
+  assert.equal(subheadings.length, 1, 'inline with defaults - renders one h3 subheading');
+
+  let subheading = subheadings[0];
+  assert.equal(subheading.innerText.toLowerCase(), 'this is a subheading', 'inline with defaults - renders correct text');
+  assert.equal(subheading.attributes['aria-label'].value, 'This is a subheading', 'inline with defaults - adds correct label');
 
   // Block form with element specified.
   this.render(hbs`
@@ -19,9 +23,12 @@ test('it renders the correct HTML', function(assert) {
       This is an underlined subheading
     {{/polaris-subheading}}
   `);
-  $subheadings = this.$(' > u.Polaris-Subheading');
-  assert.equal($subheadings.length, 1, 'block with customisation - renders one underlined subheading');
-  assert.equal($subheadings.text().trim(), 'This is an underlined subheading', 'block with customisation - renders correct text');
+
+  subheadings = findAll('u.Polaris-Subheading');
+  assert.equal(subheadings.length, 1, 'block with customisation - renders one underlined subheading');
+
+  subheading = subheadings[0];
+  assert.equal(subheading.innerText.toLowerCase(), 'this is an underlined subheading', 'block with customisation - renders correct text');
   // TODO: the react component uses the block content as the `aria-label`, but there's no clean way to do this in Ember...
-  // assert.equal($subheadings.attr('aria-label'), 'This is an underlined subheading', 'block with customisation - adds correct label');
+  // assert.equal($subheadings.attributes['aria-label'].value, 'This is an underlined subheading', 'block with customisation - adds correct label');
 });
