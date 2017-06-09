a57915309654ee677b35fe964dcbe19febd9c9b2
Andy Pye <apye@sweettoothhq.com>
2017-06-09 14:31:32 +0300

    Push version tags on release

--- config/release.js
+++ config/release.js
@@ -3,8 +3,6 @@
 
 // For details on each option run `ember help release` or check https://github.com/lytics/ember-cli-release#options.
 module.exports = {
-  // Whether to only create the git tag locally or not.
-  local: true,
   annotation: "Release %@",
   message: "Bumped version to %@",
   manifest: [ 'package.json' ],
