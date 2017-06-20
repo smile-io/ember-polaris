9e0c9426076b2874fe14cffa322f650fb4ae4559
Vlad Vanca <vanca.vlad@gmail.com>
2017-06-20 15:29:56 +0300

    Add ember-polaris blueprint

---
+++ blueprints/ember-polaris/index.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+  description: ''
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall: function(options) {
+  //   // Perform extra work here.
+  // }
+};
