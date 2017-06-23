5b72d003c8595e5bcf123a0f2ba14cfe8368755b
Andy Pye <apye@sweettoothhq.com>
2017-06-23 12:45:07 +0300

    Add documentation for section component attributes

--- addon/components/polaris-card/section.js
+++ addon/components/polaris-card/section.js
@@ -13,7 +13,37 @@ export default Component.extend({
 
   layout,
 
+  /*
+   * Public attributes.
+   */
+  /**
+   * Title for the section
+   *
+   * @property title
+   * @type {string}
+   * @default: null
+   */
   title: null,
+
+  /**
+   * A less prominent section
+   *
+   * @property subdued
+   * @type {boolean}
+   * @default: false
+   */
   subdued: false,
+
+  /**
+   * Inner content of the section
+   *
+   * This component can be used in block form,
+   * in which case the block content will be used
+   * instead of `text`
+   *
+   * @property text
+   * @type {string}
+   * @default: null
+   */
   text: null,
 });
--- addon/components/polaris-layout/annotated-section.js
+++ addon/components/polaris-layout/annotated-section.js
@@ -10,7 +10,37 @@ export default Component.extend({
 
   layout,
 
+  /*
+   * Public attributes.
+   */
+  /**
+   * Title for the section
+   *
+   * @property title
+   * @type {string}
+   * @default: null
+   */
   title: null,
+
+  /**
+   * Description for the section
+   *
+   * @property description
+   * @type {string}
+   * @default: null
+   */
   description: null,
+
+  /**
+   * Inner content of the section
+   *
+   * This component can be used in block form,
+   * in which case the block content will be used
+   * instead of `text`
+   *
+   * @property text
+   * @type {string}
+   * @default: null
+   */
   text: null,
 });
