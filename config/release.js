0dca828da1252925476bb0730590820d7abcce4d
Vlad Vanca <vanca.vlad@gmail.com>
2017-07-06 11:56:35 +0300

    Tweak ember-release config...be explicit about more options

--- config/release.js
+++ config/release.js
@@ -1,11 +1,13 @@
 /* jshint node:true */
-// var RSVP = require('rsvp');
 
-// For details on each option run `ember help release` or check https://github.com/lytics/ember-cli-release#options.
+// For details on each option run `ember help release`
 module.exports = {
+  local: false,
+  remote: 'origin',
   annotation: "Release %@",
   message: "Bumped version to %@",
   manifest: [ 'package.json' ],
   // Don't publish to npm
   publish: false,
+  strategy: 'semver'
 };
