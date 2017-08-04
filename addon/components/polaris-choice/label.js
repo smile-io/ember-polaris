91c3f2dbd9055fd9737253f3f41e4da8ea4e7733
Andy Pye <apye@sweettoothhq.com>
2017-08-04 17:30:38 +0300

    Add polaris-choice/label component

---
+++ addon/components/polaris-choice/label.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+import layout from '../../templates/components/polaris-choice/label';
+
+const {
+  Component,
+} = Ember;
+
+export default Component.extend({
+  tagName: 'label',
+  classNames: ['Polaris-Choice'],
+  classNameBindings: ['labelHidden:Polaris-Choice--labelHidden'],
+  attributeBindings: ['inputId:for'],
+
+  layout,
+
+  /*
+   * Public attributes.
+   */
+  /**
+   * ID of the input this label is for.
+   *
+   * @property inputId
+   * @type {string}
+   * @default: null
+   */
+  inputId: null,
+
+  /**
+   * Label content for the choice this label belongs to.
+   *
+   * @property label
+   * @type {string}
+   * @default: null
+   */
+  label: null,
+
+  /**
+   * Flag to hide the label
+   *
+   * @property labelHidden
+   * @type {boolean}
+   * @default: false
+   */
+  labelHidden: false,
+});
