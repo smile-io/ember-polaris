b36e65911c6dee0cdff30e3c4eb066fd718b7396
Andy Pye <apye@sweettoothhq.com>
2018-01-30 10:56:51 +0200

    Update comment for clarity

--- addon/components/wrapper-element.js
+++ addon/components/wrapper-element.js
@@ -2,8 +2,8 @@ import Component from '@ember/component';
 import layout from '../templates/components/wrapper-element';
 
 /**
- * This is a simple utility component that lets us wrap block content
- * in e.g. a conditional element, since `tagName` must be constant.
+ * This is a simple utility component that lets us do things like wrap block content
+ * in a conditional element, since `tagName` can't be a computed property.
  */
 export default Component.extend({
   layout,
