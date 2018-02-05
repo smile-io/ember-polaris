0677e4a9aa51e1b931752beb16451ef68f0f1b2c
Andy Pye <apye@sweettoothhq.com>
2018-02-05 12:50:26 +0200

    Add onClick handler to link

--- addon/utils/map-event-to-action.js
+++ addon/utils/map-event-to-action.js
@@ -1,9 +1,20 @@
 import { invokeAction } from 'ember-invoke-action';
+import { assign } from '@ember/polyfills';
 
-export default function mapEventToAction(actionName, options = { preventDefault: true }, ...args) {
+const defaultOptions = {
+  preventDefault: true,
+  stopPropagation: false,
+};
+
+export default function mapEventToAction(actionName, options, ...args) {
+  options = assign({}, defaultOptions, options);
   return function(event) {
     if (options.preventDefault) {
       event.preventDefault();
+    }
+
+    if (options.stopPropagation) {
+      event.stopPropagation();
     }
 
     return invokeAction(this, actionName, ...args);
