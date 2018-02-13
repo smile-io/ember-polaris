84471de8ff2e4d5406d3bfd32786575d17572e1f
Andy Pye <apye@sweettoothhq.com>
2018-02-13 15:38:30 +0200

    Add missing loading class to polaris-button

--- tests/integration/components/polaris-button-test.js
+++ tests/integration/components/polaris-button-test.js
@@ -354,6 +354,8 @@ test('handles the loading flag correctly', function(assert) {
 
   // Before setting the loading flag, the button should show text, icon and disclosure icon.
   let button = find('button.Polaris-Button');
+  assert.notOk(button.classList.contains('Polaris-Button--loading'), 'loading set - does not have loading class');
+
   let buttonText = find(buttonTextSelector, button).innerText;
   assert.equal(buttonText, 'This is a loading button', 'loading unset - renders correct button text');
 
@@ -376,8 +378,12 @@ test('handles the loading flag correctly', function(assert) {
   // Set the button's loading flag.
   this.set('loading', true);
 
-  // Button should now display placeholders for the icon and
-  // disclosure icon, render a spinner, and be disabled.
+  // Button should now:
+  //  - have the "loading" class;
+  //  - display placeholders for the icon and disclosure icon;
+  //  - render a spinner;
+  //  - be disabled.
+  assert.ok(button.classList.contains('Polaris-Button--loading'), 'loading set - has loading class');
   const iconPlaceholderSelector = buildNestedSelector(
     buttonIconSelector,
     'span.Polaris-Icon',
