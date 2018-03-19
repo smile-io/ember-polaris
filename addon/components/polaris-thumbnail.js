5c7e57d8fb0fca0b0223547d938df60e672887e7
Tom Netzband <tomnez@gmail.com>
2018-03-19 15:16:39 -0400

    Make internal sizeClass property readOnly

--- addon/components/polaris-thumbnail.js
+++ addon/components/polaris-thumbnail.js
@@ -3,6 +3,10 @@ import { classify } from '@ember/string';
 import { computed } from '@ember/object';
 import layout from '../templates/components/polaris-thumbnail';
 
+/**
+ * Polaris thumbnail component.
+ * See https://polaris.shopify.com/components/images-and-icons/thumbnail
+ */
 export default Component.extend({
   tagName: 'span',
   classNames: ['Polaris-Thumbnail'],
@@ -47,5 +51,5 @@ export default Component.extend({
     let size = this.get('size') || 'medium';
 
     return `Polaris-Thumbnail--size${ classify(size) }`;
-  })
+  }).readOnly()
 });
