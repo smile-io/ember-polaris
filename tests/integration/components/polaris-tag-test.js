1224bc01bca2f9aede7c8ac16ef510529e88bcff
Tom Netzband <tomnez@gmail.com>
2018-03-23 10:48:13 -0400

    Add test - it correctly renders HTML with inline usage

--- tests/integration/components/polaris-tag-test.js
+++ tests/integration/components/polaris-tag-test.js
@@ -1,29 +1,49 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { find } from 'ember-native-dom-helpers';
+import { find, findAll } from 'ember-native-dom-helpers';
+import buildNestedSelector from '../../helpers/build-nested-selector';
+import MockSvgJarComponent from '../../mocks/components/svg-jar';
 
 moduleForComponent('polaris-tag', 'Integration | Component | polaris tag', {
-  integration: true
+  integration: true,
+
+  beforeEach() {
+    this.register('component:svg-jar', MockSvgJarComponent);
+  }
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+const tag = 'Wholesale';
+const componentSelector = 'span.Polaris-Tag';
+const buttonSelector = 'button.Polaris-Tag__Button';
+const iconSelector = 'span.Polaris-Icon';
 
-  // this.render(hbs`{{polaris-tag tag='howdy'}}`);
-  this.set('onRemove', function() {
-    console.log('derp');
-  });
+const tagValueSelector = buildNestedSelector(
+  componentSelector,
+  'span'
+);
+const tagButtonSelector = buildNestedSelector(
+  componentSelector,
+  buttonSelector
+);
+const tagIconSelector = buildNestedSelector(
+  buttonSelector,
+  iconSelector
+);
 
-  // Template block usage:
-  this.render(hbs`
-    {{#polaris-tag onRemove=(action onRemove)}}
-      tag name here
-    {{/polaris-tag}}
-  `);
+test('it renders the correct HTML in inline usage', function(assert) {
+  this.set('text', tag);
+  this.render(hbs`{{polaris-tag text=text}}`);
 
-  let tag = find('.Polaris-Tag');
-  console.log(tag);
-  debugger
-  assert.ok(true)
+  const tagComponent = findAll(componentSelector);
+  assert.equal(tagComponent.length, 1, 'it renders a single tag component');
+
+  const tagValue = find(tagValueSelector);
+  assert.ok(tagValue, 'it renders a span to hold the `text` value');
+  assert.equal(tagValue.textContent.trim(), tag, 'it renders the `text` value correctly');
+
+  const tagButton = find(tagButtonSelector);
+  assert.ok(tagButton, 'it renders a tag button');
+
+  const tagIcon = find(tagIconSelector);
+  assert.ok(tagIcon, 'it renders an icon inside the tag button');
 });
