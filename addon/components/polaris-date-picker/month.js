e8faf11e3d2ad173b331d6075e33941f24ac3968
Tom Netzband <tomnez@gmail.com>
2018-04-03 15:28:47 -0400

    Stand up polaris-date-picker/month component

--- addon/components/polaris-date-picker/month.js
+++ addon/components/polaris-date-picker/month.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/polaris-date-picker/month';
+import { computed } from '@ember/object';
 import {
   Months,
   Weekdays,
@@ -13,6 +14,125 @@ import {
   abbreviationForWeekday
 } from '../utils/dates';
 
+const WEEKDAYS = [
+  Weekdays.Sunday,
+  Weekdays.Monday,
+  Weekdays.Tuesday,
+  Weekdays.Wednesday,
+  Weekdays.Thursday,
+  Weekdays.Friday,
+  Weekdays.Saturday,
+];
+
 export default Component.extend({
-  layout
+  classNames: ['Polaris-DatePicker__Month'],
+  attributeBindings: ['role'],
+
+  role: 'grid',
+
+  layout,
+
+  /**
+   * @property focusedDate
+   * @public
+   * @type {Date}
+   * @default null
+   */
+  focusedDate: null,
+
+  /**
+   * @property selected
+   * @public
+   * @type {Object}
+   * @default null
+   */
+  selected: null,
+
+  /**
+   * @property hoverDate
+   * @public
+   * @type {Date}
+   * @default null
+   */
+  hoverDate: null,
+
+  /**
+   * @property month
+   * @public
+   * @type {Number}
+   * @default null
+   */
+  month: null,
+
+  /**
+   * @property year
+   * @public
+   * @type {Number}
+   * @default null
+   */
+  year: null,
+
+  /**
+   * @property disableDatesBefore
+   * @public
+   * @type {Date}
+   * @default null
+   */
+  disableDatesBefore: null,
+
+  /**
+   * @property disableDatesAfter
+   * @public
+   * @type {Date}
+   * @default null
+   */
+  disableDatesAfter: null,
+
+  /**
+   * @property allowRange
+   * @public
+   * @type {Boolean}
+   * @default false
+   */
+  allowRange: false,
+
+  /**
+   * @property onChange
+   * @public
+   * @type {Function}
+   * @default noop
+   */
+  onChange(dateRange) {},
+
+  /**
+   * @property onHover
+   * @public
+   * @type {Function}
+   * @default noop
+   */
+  onHover(hoverEnd) {},
+
+  /**
+   * @property onFocus
+   * @public
+   * @type {Function}
+   * @default noop
+   */
+  onFocus(date) {},
+
+  /**
+   * @property monthName
+   * @public
+   * @type {Function}
+   * @default noop
+   */
+  monthName(month) {},
+
+  /**
+   * @property weekdayName
+   * @public
+   * @type {Function}
+   * @default noop
+   */
+  weekdayName(weekday) {},
 });
