e87c33f1f489c0dea9daa6bff14017b3afd786df
Andy Pye <apye@sweettoothhq.com>
2018-04-10 14:23:47 +0300

    Follow ember-bootstrap's example for setting up SASS in host app

--- blueprints/ember-polaris/index.js
+++ blueprints/ember-polaris/index.js
@@ -1,16 +1,41 @@
 /* eslint-env node */
+const rsvp = require('rsvp');
+const fs = require('fs-extra');
+const path = require('path');
+const writeFile = rsvp.denodeify(fs.writeFile);
+
 module.exports = {
   description: '',
 
-  normalizeEntityName: function() {
+  normalizeEntityName() {
     // this prevents an error when the entityName is
     // not specified when running `ember g ember-polaris`
     // (since that doesn't actually matter to us)
   },
 
-  afterInstall: function(/* options */) {
+  afterInstall(/* options */) {
     return this.addPackagesToProject([
       { name: 'ember-cli-sass', target: 'latest' }
-    ]);
-  }
+    ]).then(() => {
+      return this.addStyleImportToHostApp();
+    });
+  },
+
+  addStyleImportToHostApp() {
+    let importStatement = '@import "ember-polaris";\n';
+    let stylePath = path.join('app', 'styles');
+    let file = path.join(stylePath, 'app.scss');
+
+    if (!fs.existsSync(stylePath)) {
+      fs.mkdirSync(stylePath);
+    }
+
+    if (fs.existsSync(file)) {
+      return this.insertIntoFile(file, `\n${ importStatement }`, {});
+      this.ui.writeLine(chalk.green(`Added import statement to ${ file }`));
+    } else {
+      return writeFile(file, importStatement);
+      this.ui.writeLine(chalk.green(`Created ${ file }`));
+    }
+  },
 };
