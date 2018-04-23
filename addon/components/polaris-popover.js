45471e9bf4c4620662bd23cd6df707aca7f4b6b6
Tom Netzband <tomnez@gmail.com>
2018-04-23 11:35:58 -0400

    Add preferredPosition attr to polaris-popover

--- addon/components/polaris-popover.js
+++ addon/components/polaris-popover.js
@@ -1,7 +1,11 @@
+import Ember from 'ember';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { htmlSafe } from '@ember/string';
+import { warn } from '@ember/debug';
 import layout from '../templates/components/polaris-popover';
+
+const { ViewUtils } = Ember;
 
 /**
  * Polaris popover component.
@@ -32,9 +36,8 @@ export default Component.extend({
    * The preferred direction to open the popover
    *
    * @property preferredPosition
-   * @type {enum}
+   * @type {string}
    * @default null
-   * TODO: not implemented
    */
   preferredPosition: null,
 
@@ -90,6 +93,23 @@ export default Component.extend({
   /*
    * Internal properties.
    */
+  verticalPosition: computed('preferredPosition', function() {
+    let preferredPosition = this.get('preferredPosition');
+
+    if (preferredPosition === 'above') {
+      return preferredPosition;
+    }
+
+    if (preferredPosition === 'below') {
+      return preferredPosition;
+    }
+
+    // If set to `mostSpace`, the value will be
+    // calculated when the user opens the popover.
+
+    return null;
+  }),
+
   triggerStyle: computed(function() {
     return htmlSafe(`
       display: inline-block;
@@ -97,4 +117,49 @@ export default Component.extend({
       border: none;
     `);
   }).readOnly(),
+
+  /**
+   * Checks the dropdown trigger's location on
+   * screen to determine which vertical direction
+   * has more space to open the dropdown.
+   */
+  mostVerticalSpace() {
+    let component;
+
+    // Hack to get the component's container
+    // element since this component uses `tagName: ''`
+    if (ViewUtils && ViewUtils.getViewBounds) {
+      component =  ViewUtils.getViewBounds(this).parentElement;
+    } else {
+      component = this._renderNode.contextualElement;
+    }
+
+    let triggers = component.querySelectorAll('.ember-basic-dropdown-trigger');
+
+    if (triggers.length > 1) {
+      warn('Multiple popover triggers found. Defaulting to `preferredPosition` of `below`');
+      return 'below';
+    }
+
+    let [trigger] = triggers;
+    let { top, bottom } = trigger.getBoundingClientRect();
+    let windowHeight = window.innerHeight;
+    let bottomSpace = windowHeight - bottom;
+
+    // Use `below` if distance is equal
+    return bottomSpace >= top ? 'below' : 'above';
+  },
+
+  actions: {
+    onOpen() {
+      // Check to see if `preferredPosition` is set to `mostSpace`
+      // onOpen, since user could have scrolled vertically since
+      // the time the component originally rendered.
+      let preferredPosition = this.get('preferredPosition');
+
+      if (preferredPosition === 'mostSpace') {
+        this.set('verticalPosition', this.mostVerticalSpace());
+      }
+    }
+  }
 });
