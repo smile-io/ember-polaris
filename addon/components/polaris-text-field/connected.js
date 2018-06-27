294ac2c00fed40566fecaced1214acd6d8725fcb
Tom Netzband <tomnez@gmail.com>
2018-06-27 08:54:07 -0400

    Implement polaris-text-field/connected component

---
+++ addon/components/polaris-text-field/connected.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/polaris-text-field/connected';
+
+export default Component.extend({
+  tagName: '',
+
+  layout
+});
