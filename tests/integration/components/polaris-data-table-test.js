fc289096ea27273849d7c85b508a97d8b34ffd21
Andy Pye <apye@sweettoothhq.com>
2018-06-27 16:11:42 +0300

    Use find, findAll from @ember/test-helpers instead of ember-native-dom-helpers

--- tests/integration/components/polaris-data-table-test.js
+++ tests/integration/components/polaris-data-table-test.js
@@ -1,9 +1,8 @@
 import { helper } from '@ember/component/helper';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, findAll, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import { findAll, find } from 'ember-native-dom-helpers';
 
 const sortable = [false, true, false, false, true, false];
 const columnContentTypes = [
