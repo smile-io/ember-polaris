cef4b8ce66e0cfd1621070ecfb61028e10f05e92
Andy Pye <apye@sweettoothhq.com>
2018-07-02 13:29:41 +0300

    Updates to component-proxy to unset previous properties when the props hash updates and fix linting error.

--- addon/components/component-proxy.js
+++ addon/components/component-proxy.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import Ember from 'ember';
+// import { compileTemplate } from '@ember/template-compilation';
+import { assign } from '@ember/polyfills';
+import Ember from 'ember'
 
 // Renders another component with a passed-in hash of properties.
 // This effectively allows us to spread/splat a props hash onto
@@ -32,8 +34,19 @@ export default Component.extend({
    */
   props: null,
 
+  /**
+   * List of properties that were previously set via the `props` hash.
+   * @type {Array}
+   * @private
+   */
+  propNames: null,
+
   layout: computed('componentName', 'propsString', function() {
     let { componentName, propsString } = this.getProperties('componentName', 'propsString');
+
+    // Disable linting for this line because the recommended way of importing
+    // results in a "Could not find module `@ember/template-compilation`" error ¯\_(ツ)_/¯
+    // eslint-disable-next-line ember/new-module-imports
     return Ember.HTMLBars.compile(`
       {{#if hasBlock}}
         {{#component "${ componentName }"${ propsString }}}
@@ -52,10 +65,25 @@ export default Component.extend({
     }, '');
   }).readOnly(),
 
+  updateProps() {
+    // Unset any properties that were set previously and are now undefined,
+    // spread the new passed-in props hash into our component's context,
+    // and make a note of which properties are currently set.
+    let oldPropNames = this.get('propNames') || [];
+    let props = this.get('props') || {};
+    let propNames = Object.keys(props);
+
+    let propsToUnset = oldPropNames.reduce((propsToUnset, propName) => {
+      propsToUnset[propName] = undefined;
+    }, {});
+
+    let newProps = assign(propsToUnset, props, { propNames })
+    this.setProperties(newProps);
+  },
+
   didReceiveAttrs() {
     this._super(...arguments);
 
-    // Spread the passed-in props hash into our component's context.
-    this.setProperties(this.get('props') || {});
+    this.updateProps();
   },
 });
