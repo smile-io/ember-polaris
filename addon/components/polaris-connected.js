cf4f830e9f7b54e1b8c177779af518167ae6a725
Tom Netzband <tomnez@gmail.com>
2018-07-02 15:56:39 -0400

    Add polaris-connected component

---
+++ addon/components/polaris-connected.js
@@ -0,0 +1,28 @@
+import Component from '@ember/component';
+import layout from '../templates/components/polaris-connected';
+
+export default Component.extend({
+  tagName: '',
+
+  layout,
+
+  /**
+   * An element connected to the left of the yielded content
+   *
+   * @property left
+   * @public
+   * @type {String|Component}
+   * @default null
+   */
+  left: null,
+
+  /**
+   * An element connected to the right of the yielded content
+   *
+   * @property right
+   * @public
+   * @type {String|Component}
+   * @default null
+   */
+  right: null,
+});
