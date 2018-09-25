7908f6688d1ae2a5cdf4bbc385077aee62d75ac1
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-09-25 22:45:08 +0530

    Fix template errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,8 +1,13 @@
 /* eslint-env node */
 'use strict';
 
+const OFF = false;
+
 module.exports = {
   plugins: ['./node_modules/@smile-io/ember-styleguide/.template-lintrc'],
   extends: ['@smile-io/ember-styleguide:recommended'],
-  rules: {},
+  rules: {
+    'attribute-indentation': OFF,
+    'no-inline-styles': OFF,
+  },
 };
