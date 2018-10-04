d8a24f55b1c64871237f7d4be314ed07fa3c0116
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-10-04 14:08:54 +0530

    Dont oversimplify code

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,13 +1,11 @@
 /* eslint-env node */
 'use strict';
 
-const OFF = false;
-
 module.exports = {
   plugins: ['./node_modules/@smile-io/ember-styleguide/.template-lintrc'],
   extends: ['@smile-io/ember-styleguide:recommended'],
   rules: {
-    'attribute-indentation': OFF,
-    'no-inline-styles': OFF,
+    'attribute-indentation': false,
+    'no-inline-styles': false,
   },
 };
