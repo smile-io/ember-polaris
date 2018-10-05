fc610b52dc01eb3af9157fed98b5cf4dc24778ed
Vlad Vanca <vanca.vlad@gmail.com>
2018-10-05 13:53:14 +0300

    Fix tests

--- tests/integration/components/polaris-inline-error-test.js
+++ tests/integration/components/polaris-inline-error-test.js
@@ -8,8 +8,8 @@ module('Integration | Component | polaris-inline-error', function(hooks) {
 
   test('it renders correctly', async function(assert) {
     this.setProperties({
-      message: 'Message can\'t be blank',
-      fieldID: 'uniqueID'
+      message: "Message can't be blank",
+      fieldID: 'uniqueID',
     });
 
     await render(hbs`
@@ -20,20 +20,36 @@ module('Integration | Component | polaris-inline-error', function(hooks) {
     `);
 
     assert.dom('[data-test-inline-error]').exists();
-    assert.dom('[data-test-inline-error]').hasClass('Polaris-InlineError', 'has correct class applied');
-    assert.dom('[data-test-inline-error]').hasText(this.message, 'has correct message');
-    assert.dom('[data-test-inline-error]').hasAttribute('id', `${this.fieldID}Error`, 'has ID generated from the fieldID');
-    assert.dom('[data-test-inline-error-icon]').exists('renders inline error icon');
-    assert.dom('[data-test-inline-error-icon]').hasClass('Polaris-InlineError__Icon', 'error icon has correct class');
+    assert
+      .dom('[data-test-inline-error]')
+      .hasClass('Polaris-InlineError', 'has correct class applied');
+    assert
+      .dom('[data-test-inline-error]')
+      .hasText(this.message, 'has correct message');
+    assert
+      .dom('[data-test-inline-error]')
+      .hasAttribute(
+        'id',
+        `${this.fieldID}Error`,
+        'has ID generated from the fieldID'
+      );
+    assert
+      .dom('[data-test-inline-error-icon]')
+      .exists('renders inline error icon');
+    assert
+      .dom('[data-test-inline-error-icon]')
+      .hasClass('Polaris-InlineError__Icon', 'error icon has correct class');
 
     this.set('message', null);
-    assert.dom('[data-test-inline-error]').doesNotExist('does not renders when message is not truthy');
+    assert
+      .dom('[data-test-inline-error]')
+      .doesNotExist('does not renders when message is not truthy');
   });
 
   test('works with `message` being a component', async function(assert) {
     this.setProperties({
-      message: 'Message can\'t be blank',
-      fieldID: 'uniqueID'
+      message: "Message can't be blank",
+      fieldID: 'uniqueID',
     });
 
     await render(hbs`
@@ -43,6 +59,8 @@ module('Integration | Component | polaris-inline-error', function(hooks) {
       }}
     `);
 
-    assert.dom('[data-test-inline-error] > .Polaris-DisplayText').exists('renders `message` as a component');
+    assert
+      .dom('[data-test-inline-error] > .Polaris-DisplayText')
+      .exists('renders `message` as a component');
   });
 });
