033ba42e1c29af9cdbaf37c1f6ea7fd193265109
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-10-09 15:30:23 +0530

    Toggling in a backward compatible way

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,14 +1,5 @@
-import Controller, { inject } from '@ember/controller';
+import Controller, { inject as controller } from '@ember/controller';
 
 export default Controller.extend({
-  applicationController: inject('application'),
-
-  actions: {
-    toggleMagicSection() {
-      this.applicationController.set(
-        'hideMagicSection',
-        !this.applicationController.hideMagicSection
-      );
-    },
-  },
+  application: controller(),
 });
