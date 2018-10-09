07c8ce58b60fdf04daf2d63b214f39299f2e5383
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-10-09 18:43:41 +0530

    Add better self-explanatory assert msgs

--- tests/acceptance/key-event-listener-test.js
+++ tests/acceptance/key-event-listener-test.js
@@ -3,26 +3,36 @@ import { setupApplicationTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 import { keyDown } from 'ember-keyboard/test-support/test-helpers';
 
-module('Acceptance | key press listener test', function(hooks) {
+module('Acceptance | key event listener test', function(hooks) {
   setupApplicationTest(hooks);
 
   test('hidden easter egg works with key m only on index page', async function(assert) {
     await visit('/');
 
     let easterEggBanner = '[data-test-banner="easter-egg-banner"]';
-    assert.dom(easterEggBanner).doesNotExist();
+    assert
+      .dom(easterEggBanner)
+      .doesNotExist(`Easter egg banner should be hidden by default`);
 
     await keyDown('KeyM');
-    assert.dom(easterEggBanner).exists();
+    assert
+      .dom(easterEggBanner)
+      .exists(`Easter egg banner should appear when m is pressed`);
 
     await visit('/test');
     await keyDown('KeyM');
-    assert.dom(easterEggBanner).exists(`Event doesn't trigger on other pages`);
+    assert
+      .dom(easterEggBanner)
+      .exists(
+        `Easter egg banner doesn't hide when pressing m on a page w/o the key-event-listener`
+      );
 
     await visit('/');
     await keyDown('KeyM');
     assert
       .dom(easterEggBanner)
-      .doesNotExist(`Event doesn't trigger on other pages`);
+      .doesNotExist(
+        `Easter egg banner is hidden when pressing m on index page which has key-event-listener`
+      );
   });
 });
