9becc619fb4fdeadcebd0e7c2e9cfe6d04fc4a26
Vlad Vanca <vanca.vlad@gmail.com>
2018-10-11 20:07:58 +0300

    Fix linting error

--- tests/integration/components/polaris-progress-bar-test.js
+++ tests/integration/components/polaris-progress-bar-test.js
@@ -1,7 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import { find } from 'ember-native-dom-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import buildNestedSelector from '../../helpers/build-nested-selector';
 
