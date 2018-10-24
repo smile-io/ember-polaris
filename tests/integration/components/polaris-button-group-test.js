c762a04701766ad14bc563e454c269ce190a1a8d
Tom Netzband <tomnez@gmail.com>
2018-10-24 12:43:31 -0400

    Use dom assertions in test file

--- tests/integration/components/polaris-button-group-test.js
+++ tests/integration/components/polaris-button-group-test.js
@@ -42,16 +42,20 @@ test('it renders the correct HTML with segmented attribute', function(assert) {
   this.render(hbs`{{polaris-button-group segmented=segmented}}`);
 
   const buttonGroup = find(buttonGroupSelector);
-  assert.ok(
-    buttonGroup.classList.contains('Polaris-ButtonGroup--segmented'),
-    'segmented=true - adds the segmented class'
-  );
+  assert
+    .dom(buttonGroup)
+    .hasClass(
+      'Polaris-ButtonGroup--segmented',
+      'segmented=true - adds the segmented class'
+    );
 
   this.set('segmented', false);
-  assert.notOk(
-    buttonGroup.classList.contains('Polaris-ButtonGroup--segmented'),
-    'segmented=true - does not add the segmented class'
-  );
+  assert
+    .dom(buttonGroup)
+    .hasNoClass(
+      'Polaris-ButtonGroup--segmented',
+      'segmented=true - does not add the segmented class'
+    );
 });
 
 test('it renders the correct HTML with fullWidth attribute', function(assert) {
@@ -59,16 +63,20 @@ test('it renders the correct HTML with fullWidth attribute', function(assert) {
   this.render(hbs`{{polaris-button-group fullWidth=fullWidth}}`);
 
   const buttonGroup = find(buttonGroupSelector);
-  assert.ok(
-    buttonGroup.classList.contains('Polaris-ButtonGroup--fullWidth'),
-    'fullWidth=true - adds the fullWidth class'
-  );
+  assert
+    .dom(buttonGroup)
+    .hasClass(
+      'Polaris-ButtonGroup--fullWidth',
+      'fullWidth=true - adds the fullWidth class'
+    );
 
   this.set('fullWidth', false);
-  assert.notOk(
-    buttonGroup.classList.contains('Polaris-ButtonGroup--fullWidth'),
-    'fullWidth=false - does not add the fullWidth class'
-  );
+  assert
+    .dom(buttonGroup)
+    .hasNoClass(
+      'Polaris-ButtonGroup--fullWidth',
+      'fullWidth=false - does not add the fullWidth class'
+    );
 });
 
 test('it renders the correct HTML with connectedTop attribute', function(assert) {
@@ -76,16 +84,20 @@ test('it renders the correct HTML with connectedTop attribute', function(assert)
   this.render(hbs`{{polaris-button-group connectedTop=connectedTop}}`);
 
   const buttonGroup = find(buttonGroupSelector);
-  assert.ok(
-    buttonGroup.classList.contains('Polaris-ButtonGroup--connectedTop'),
-    'connectedTop=true - adds the connectedTop class'
-  );
+  assert
+    .dom(buttonGroup)
+    .hasClass(
+      'Polaris-ButtonGroup--connectedTop',
+      'connectedTop=true - adds the connectedTop class'
+    );
 
   this.set('connectedTop', false);
-  assert.notOk(
-    buttonGroup.classList.contains('Polaris-ButtonGroup--connectedTop'),
-    'connectedTop=false - does not add the connectedTop class'
-  );
+  assert
+    .dom(buttonGroup)
+    .hasNoClass(
+      'Polaris-ButtonGroup--connectedTop',
+      'connectedTop=false - does not add the connectedTop class'
+    );
 });
 
 test('it renders the correct HTML in block usage', function(assert) {
@@ -110,39 +122,45 @@ test('it renders the correct HTML in block usage', function(assert) {
 
   // Check the first item.
   let buttonGroupItem = buttonGroupItems[0];
-  assert.ok(
-    buttonGroupItem.classList.contains('Polaris-ButtonGroup__Item--plain'),
-    'first group item - has plain class'
-  );
-  assert.equal(
-    buttonGroupItem.textContent.trim(),
-    'Plain block item',
-    'first group item - renders the correct content'
-  );
+  assert
+    .dom(buttonGroupItem)
+    .hasClass(
+      'Polaris-ButtonGroup__Item--plain',
+      'first group item - has plain class'
+    );
+  assert
+    .dom(buttonGroupItem)
+    .hasText(
+      'Plain block item',
+      'first group item - renders the correct content'
+    );
 
   // Check the second item.
   buttonGroupItem = buttonGroupItems[1];
-  assert.notOk(
-    buttonGroupItem.classList.contains('Polaris-ButtonGroup__Item--plain'),
-    'second group item - does not have plain class'
-  );
-  assert.equal(
-    buttonGroupItem.textContent.trim(),
-    'Inline item',
-    'second group item - renders the correct content'
-  );
+  assert
+    .dom(buttonGroupItem)
+    .hasNoClass(
+      'Polaris-ButtonGroup__Item--plain',
+      'second group item - does not have plain class'
+    );
+  assert
+    .dom(buttonGroupItem)
+    .hasText('Inline item', 'second group item - renders the correct content');
 
   // Check the third item.
   buttonGroupItem = buttonGroupItems[2];
-  assert.notOk(
-    buttonGroupItem.classList.contains('Polaris-ButtonGroup__Item--plain'),
-    'third group item - does not have plain class'
-  );
-  assert.equal(
-    buttonGroupItem.textContent.trim(),
-    'Magically wrapped button',
-    'third group item - renders the correct content'
-  );
+  assert
+    .dom(buttonGroupItem)
+    .hasNoClass(
+      'Polaris-ButtonGroup__Item--plain',
+      'third group item - does not have plain class'
+    );
+  assert
+    .dom(buttonGroupItem)
+    .hasText(
+      'Magically wrapped button',
+      'third group item - renders the correct content'
+    );
 });
 
 test('it handles focused buttons correctly', function(assert) {
@@ -157,20 +175,26 @@ test('it handles focused buttons correctly', function(assert) {
   const buttonGroupItem = find(buttonGroupItemSelector);
   assert.ok(buttonGroupItem, 'renders the button group item');
 
-  assert.notOk(
-    buttonGroupItem.classList.contains('Polaris-ButtonGroup__Item--focused'),
-    'before focus - group item does not have focused class'
-  );
+  assert
+    .dom(buttonGroupItem)
+    .hasNoClass(
+      'Polaris-ButtonGroup__Item--focused',
+      'before focus - group item does not have focused class'
+    );
 
   focus('.Polaris-Button');
-  assert.ok(
-    buttonGroupItem.classList.contains('Polaris-ButtonGroup__Item--focused'),
-    'after focus - group item has focused class'
-  );
+  assert
+    .dom(buttonGroupItem)
+    .hasClass(
+      'Polaris-ButtonGroup__Item--focused',
+      'after focus - group item has focused class'
+    );
 
   blur('.Polaris-Button');
-  assert.notOk(
-    buttonGroupItem.classList.contains('Polaris-ButtonGroup__Item--focused'),
-    'after blur - group item does not have focused class'
-  );
+  assert
+    .dom(buttonGroupItem)
+    .hasNoClass(
+      'Polaris-ButtonGroup__Item--focused',
+      'after blur - group item does not have focused class'
+    );
 });
