83157f4f993d37be068ae71f48b7a450ad5aedf9
Andy Pye <apye@sweettoothhq.com>
2018-11-28 17:41:34 +0200

    Finish initial build of filter value selector

--- addon/components/polaris-resource-list/filter-control/filter-value-selector.js
+++ addon/components/polaris-resource-list/filter-control/filter-value-selector.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { computed, get } from '@ember/object';
 import layout from '../../../templates/components/polaris-resource-list/filter-control/filter-value-selector';
 
 const FilterType = {
@@ -53,4 +54,60 @@ export default Component.extend({
    * @public
    */
   onFilterKeyChange() {},
+
+  showOperatorOptions: computed('filter.{type,operatorText}', function() {
+    let filter = this.get('filter');
+    let type = get(filter, 'type');
+    let operatorText = get(filter, 'operatorText');
+
+    return (
+      type !== FilterType.DateSelector &&
+      operatorText &&
+      typeof operatorText !== 'string'
+    );
+  }).readOnly(),
+
+  operatorOptions: computed(
+    'filter.{label,operatorText}',
+    'filterKey',
+    function() {
+      let { filter, filterKey } = this.getProperties('filter', 'filterKey');
+
+      return {
+        componentName: 'polaris-select',
+        props: {
+          labelHidden: true,
+          label: get(filter, 'label'),
+          options: buildOperatorOptions(get(filter, 'operatorText')),
+          value: filterKey,
+          onChange: (...args) => this.handleOperatorOptionChange(...args),
+        },
+      };
+    }
+  ).readOnly(),
+
+  handleOperatorOptionChange(operatorKey) {
+    let { value, onChange, onFilterKeyChange } = this.getProperties(
+      'value',
+      'onChange',
+      'onFilterKeyChange'
+    );
+    onFilterKeyChange(operatorKey);
+
+    if (!value) {
+      return;
+    }
+
+    onChange(value);
+  },
 });
+
+function buildOperatorOptions(operatorText) {
+  if (!operatorText || typeof operatorText === 'string') {
+    return [];
+  }
+
+  return operatorText.map(({ key, optionLabel }) => {
+    return { value: key, label: optionLabel };
+  });
+}
