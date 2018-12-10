344e92b7baaf4058e97c711e267d6948327bd90b
Andy Pye <apye@sweettoothhq.com>
2018-12-10 16:39:59 +0200

    Begin overhauling resource list with new interface

--- addon/components/polaris-resource-list.js
+++ addon/components/polaris-resource-list.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { assert } from '@ember/debug';
 import layout from '../templates/components/polaris-resource-list';
 
 /**
@@ -6,14 +7,10 @@ import layout from '../templates/components/polaris-resource-list';
  * See https://polaris.shopify.com/components/lists/resource-list
  */
 export default Component.extend({
-  tagName: 'ul',
-
-  classNames: ['Polaris-ResourceList'],
-
   layout,
 
   /**
-   * Item data; each item is rendered using the itemComponent below
+   * Item data; each item is passed to renderItem
    *
    * @property items
    * @type {Array}
@@ -23,12 +20,201 @@ export default Component.extend({
   items: null,
 
   /**
-   * Component to render each item
-   *
-   * @property itemComponent
-   * @type {function}
-   * @default 'polaris-resource-list/item'
+   * @property filterControl
+   * @type {String|Component|Object}
+   * @default null
    * @public
    */
-  itemComponent: 'polaris-resource-list/item',
+  filterControl: null,
+
+  /**
+   * Name of the resource, such as customers or products
+   * Object with `singular` and `plural` properties
+   *
+   * @property resourceName
+   * @type {Object}
+   * @default null
+   * @public
+   */
+  resourceName: null,
+
+  /**
+   * Up to 2 bulk actions that will be given more prominence
+   * Array of objects with properties:
+   *  id
+   *  content
+   *  accessibilityLabel
+   *  url
+   *  external
+   *  disabled
+   *  onAction
+   *
+   * @property promotedBulkActions
+   * @type {Object[]}
+   * @default null
+   * @public
+   */
+  promotedBulkActions: null,
+
+  /**
+   * Actions available on the currently selected items
+   * Array of action objects with properties:
+   *  id
+   *  content
+   *  accessibilityLabel
+   *  url
+   *  external
+   *  disabled
+   *  onAction
+   *
+   * or section object with properties:
+   *  title
+   *  items
+   *
+   * where items is a list of action objects
+   *
+   * @property bulkActions
+   * @type {Object[]}
+   * @default null
+   * @public
+   */
+  bulkActions: null,
+
+  /**
+   * Collection of IDs for the currently selected items
+   * Can be either an array of IDs, or the string literal 'All'
+   *
+   * @property selectedItems
+   * @type {String|String[]}
+   * @default null
+   * @public
+   */
+  selectedItems: null,
+
+  /**
+   * If there are more items than currently in the list
+   *
+   * @property hasMoreItems
+   * @type {Boolean}
+   * @default false
+   * @public
+   */
+  hasMoreItems: false,
+
+  /**
+   * Overlays item list with a spinner while a background action is being performed
+   *
+   * @property loading
+   * @type {Boolean}
+   * @default false
+   * @public
+   */
+  loading: false,
+
+  /**
+   * Boolean to show or hide the header
+   *
+   * @property showHeader
+   * @type {Boolean}
+   * @default false
+   * @public
+   */
+  showHeader: false,
+
+  /**
+   * Current value of the sort control
+   *
+   * @property sortValue
+   * @type {String}
+   * @default null
+   * @public
+   */
+  sortValue: null,
+
+  /**
+   * Collection of sort options to choose from
+   * Properties are the same as for polaris-select's options
+   *
+   * @property sortOptions
+   * @type {(String|Object)[]}
+   * @default null
+   * @public
+   */
+  sortOptions: null,
+
+  /**
+   * Component to render each list item
+   * This is in place of the React implementation's
+   * renderItem property
+   *
+   * @property itemComponent
+   * @type {String|Component|Object}
+   * @default null
+   * @public
+   * @required
+   */
+  itemComponent: null,
+
+  /**
+   * Callback when sort option is changed
+   *
+   * @property onSortChange
+   * @type {Function}
+   * @default noop
+   * @public
+   */
+  onSortChange() {},
+
+  /**
+   * Callback when selection is changed
+   *
+   * @property onSelectionChange
+   * @type {Function}
+   * @default noop
+   * @public
+   */
+  onSelectionChange() {},
+
+  /**
+   * Function to customize the unique ID for each item
+   *
+   * @property idForItem
+   * @type {Function}
+   * @default noop
+   * @public
+   */
+  idForItem() {},
+
+  /**
+   * @property selectMode
+   * @type {Boolean}
+   * @default false
+   * @private
+   */
+  selectMode: false,
+
+  /**
+   * @property loadingPosition
+   * @type {Number}
+   * @default 0
+   * @private
+   */
+  loadingPosition: 0,
+
+  /**
+   * @property listNode
+   * @type {HTMLUListElement}
+   * @default null
+   * @private
+   */
+  listNode: null,
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    assert(
+      'ember-polaris::polaris-resource-list - itemComponent must be a component name or definition',
+      this.get('itemComponent')
+    );
+  },
 });
