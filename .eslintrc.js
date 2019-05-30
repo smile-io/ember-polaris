3cdb2030097d959b12f41d9a64ab8ec3b216ea0b
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-05-30 09:11:45 +0530

    Update smile linting & code formatting tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,49 +2,7 @@
 'use strict';
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['ember', 'smile-ember'],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:smile-ember/addon',
-  ],
-  env: {
-    browser: true,
-  },
+  plugins: ['smile-ember'],
+  extends: ['plugin:smile-ember/addon'],
   rules: {},
-  overrides: [
-    // node files
-    {
-      files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'index.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'tests/dummy/config/**/*.js',
-      ],
-      excludedFiles: [
-        'addon/**',
-        'addon-test-support/**',
-        'app/**',
-        'tests/dummy/app/**',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015,
-      },
-      env: {
-        browser: false,
-        node: true,
-      },
-      plugins: ['node'],
-    },
-  ],
 };
