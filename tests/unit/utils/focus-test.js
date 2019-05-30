2e4be3cc5b1457e285ded9a8dfd058a3091df8f1
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-05-30 11:03:24 +0530

    Migrate unit tests to new test module syntax

--- tests/unit/utils/focus-test.js
+++ tests/unit/utils/focus-test.js
@@ -1,19 +1,19 @@
 import focus from 'dummy/utils/focus';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | focus');
-
-test("handleMouseUpByBlurring calls blur on the passed in event's currentTarget", function(assert) {
-  let blurCalled = false;
-  let mockEvent = {
-    currentTarget: {
-      blur() {
-        blurCalled = true;
+module('Unit | Utility | focus', function() {
+  test("handleMouseUpByBlurring calls blur on the passed in event's currentTarget", function(assert) {
+    let blurCalled = false;
+    let mockEvent = {
+      currentTarget: {
+        blur() {
+          blurCalled = true;
+        },
       },
-    },
-  };
+    };
 
-  focus.handleMouseUpByBlurring(mockEvent);
+    focus.handleMouseUpByBlurring(mockEvent);
 
-  assert.ok(blurCalled, "blur called on event's currentTarget");
+    assert.ok(blurCalled, "blur called on event's currentTarget");
+  });
 });
