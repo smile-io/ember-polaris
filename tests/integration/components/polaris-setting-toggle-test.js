aad39ca8e37f16b84f267164924ee36b9fc1b439
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-05-30 13:15:27 +0530

    Refactor to use verbose qunit dom apis

--- tests/integration/components/polaris-setting-toggle-test.js
+++ tests/integration/components/polaris-setting-toggle-test.js
@@ -1,200 +1,183 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import { findAll, find, click } from 'ember-native-dom-helpers';
 import buildNestedSelector from '../../helpers/build-nested-selector';
 
-moduleForComponent(
-  'polaris-setting-toggle',
-  'Integration | Component | polaris setting toggle',
-  {
-    integration: true,
-  }
-);
+module('Integration | Component | polaris setting toggle', function(hooks) {
+  setupRenderingTest(hooks);
 
-const settingActionSelector = buildNestedSelector(
-  'div.Polaris-Card',
-  'div.Polaris-Card__Section',
-  'div.Polaris-SettingAction'
-);
-const settingActionSettingSelector = buildNestedSelector(
-  settingActionSelector,
-  'div.Polaris-SettingAction__Setting'
-);
-const settingActionWrapperSelector = buildNestedSelector(
-  settingActionSelector,
-  'div.Polaris-SettingAction__Action'
-);
-const settingActionButtonSelector = buildNestedSelector(
-  settingActionWrapperSelector,
-  'button.Polaris-Button'
-);
-
-test('it renders the correct HTML in inline usage with default attributes', function(assert) {
-  this.render(hbs`{{polaris-setting-toggle text="Inline setting toggle"}}`);
-
-  const settingActions = findAll(settingActionSelector);
-  assert.equal(settingActions.length, 1, 'renders one setting action');
-
-  const settingActionSettings = findAll(settingActionSettingSelector);
-  assert.equal(
-    settingActionSettings.length,
-    1,
-    'renders one setting action setting'
+  const settingActionSelector = buildNestedSelector(
+    'div.Polaris-Card',
+    'div.Polaris-Card__Section',
+    'div.Polaris-SettingAction'
   );
-  assert.equal(
-    settingActionSettings[0].textContent.trim(),
-    'Inline setting toggle',
-    'renders the correct setting action setting text'
+  const settingActionSettingSelector = buildNestedSelector(
+    settingActionSelector,
+    'div.Polaris-SettingAction__Setting'
+  );
+  const settingActionWrapperSelector = buildNestedSelector(
+    settingActionSelector,
+    'div.Polaris-SettingAction__Action'
+  );
+  const settingActionButtonSelector = buildNestedSelector(
+    settingActionWrapperSelector,
+    'button.Polaris-Button'
   );
 
-  const settingActionWrappers = findAll(settingActionWrapperSelector);
-  assert.equal(
-    settingActionWrappers.length,
-    1,
-    'renders one setting action wrapper'
-  );
+  test('it renders the correct HTML in inline usage with default attributes', async function(assert) {
+    await render(hbs`{{polaris-setting-toggle text="Inline setting toggle"}}`);
 
-  const settingActionButtons = findAll(settingActionButtonSelector);
-  assert.equal(
-    settingActionButtons.length,
-    0,
-    'does not render any setting action buttons'
-  );
-});
+    assert
+      .dom(settingActionSelector)
+      .exists({ count: 1 }, 'renders one setting action');
 
-test('it renders the correct HTML in block usage with action supplied', function(assert) {
-  this.render(hbs`
-    {{#polaris-setting-toggle
-      action=(hash
-        text="Take action!"
-      )
-    }}
-      Block setting toggle
-    {{/polaris-setting-toggle}}
-  `);
+    const settingActionSettings = assert.dom(settingActionSettingSelector);
+    settingActionSettings.exists(
+      { count: 1 },
+      'renders one setting action setting'
+    );
+    settingActionSettings.hasText(
+      'Inline setting toggle',
+      'renders the correct setting action setting text'
+    );
 
-  const settingActions = findAll(settingActionSelector);
-  assert.equal(settingActions.length, 1, 'renders one setting action');
+    assert
+      .dom(settingActionWrapperSelector)
+      .exists({ count: 1 }, 'renders one setting action wrapper');
 
-  const settingActionSettings = findAll(settingActionSettingSelector);
-  assert.equal(
-    settingActionSettings.length,
-    1,
-    'renders one setting action setting'
-  );
-  assert.equal(
-    settingActionSettings[0].textContent.trim(),
-    'Block setting toggle',
-    'renders the correct setting action setting content'
-  );
-
-  const settingActionWrappers = findAll(settingActionWrapperSelector);
-  assert.equal(
-    settingActionWrappers.length,
-    1,
-    'renders one setting action wrapper'
-  );
-
-  const settingActionButtons = findAll(settingActionButtonSelector);
-  assert.equal(
-    settingActionButtons.length,
-    1,
-    'renders one setting action button'
-  );
-
-  const settingActionButton = settingActionButtons[0];
-  assert.equal(
-    settingActionButton.textContent.trim(),
-    'Take action!',
-    'renders the correct setting action button content'
-  );
-  assert.notOk(
-    settingActionButton.classList.contains('Polaris-Button--primary'),
-    'renders plain setting action button'
-  );
-});
-
-test('it handles the enabled attribute correctly', function(assert) {
-  this.set('enabled', true);
-  this.render(hbs`
-    {{polaris-setting-toggle
-      enabled=enabled
-      action=(hash
-        text="Flip the switch"
-      )
-    }}
-  `);
-
-  const settingActionButton = find(settingActionButtonSelector);
-  assert.ok(settingActionButton, 'renders the setting action button');
-  assert.equal(
-    settingActionButton.textContent.trim(),
-    'Flip the switch',
-    'renders the correct setting action button content'
-  );
-
-  assert.ok(
-    settingActionButton.classList.contains('Polaris-Button--primary'),
-    'with enabled true - renders primary setting action button'
-  );
-
-  this.set('enabled', false);
-  assert.notOk(
-    settingActionButton.classList.contains('Polaris-Button--primary'),
-    'with enabled false - renders plain setting action button'
-  );
-});
-
-test('it handles the supplied action correctly', function(assert) {
-  this.set('actionFired', false);
-  this.render(hbs`
-    {{polaris-setting-toggle
-      enabled=enabled
-      action=(hash
-        text="Flip the switch"
-        onAction=(action (mut actionFired) true)
-      )
-    }}
-  `);
-
-  click(settingActionButtonSelector);
-  assert.ok(this.get('actionFired'), 'fires the action when button clicked');
-});
-
-test("it obeys the passed-in action hash's loading and disabled flags", function(assert) {
-  this.setProperties({
-    isLoading: true,
-    isDisabled: false,
+    assert
+      .dom(settingActionButtonSelector)
+      .doesNotExist('does not render any setting action buttons');
   });
 
-  this.render(hbs`
-    {{polaris-setting-toggle
-      action=(hash
-        text="Toggle"
-        loading=isLoading
-        disabled=isDisabled
-        onAction=(action (mut dummy))
-      )
-    }}
-  `);
+  test('it renders the correct HTML in block usage with action supplied', async function(assert) {
+    await render(hbs`
+      {{#polaris-setting-toggle
+        action=(hash
+          text="Take action!"
+        )
+      }}
+        Block setting toggle
+      {{/polaris-setting-toggle}}
+    `);
 
-  let button = find(settingActionButtonSelector);
-  assert.ok(
-    button.classList.contains('Polaris-Button--loading'),
-    'button is in loading state when loading is true'
-  );
+    assert
+      .dom(settingActionSelector)
+      .exists({ count: 1 }, 'renders one setting action');
 
-  this.setProperties({
-    isLoading: false,
-    isDisabled: true,
+    const settingActionSettings = assert.dom(settingActionSettingSelector);
+    settingActionSettings.exists(
+      { count: 1 },
+      'renders one setting action setting'
+    );
+    settingActionSettings.hasText(
+      'Block setting toggle',
+      'renders the correct setting action setting content'
+    );
+
+    assert
+      .dom(settingActionWrapperSelector)
+      .exists({ count: 1 }, 'renders one setting action wrapper');
+
+    const settingActionButtons = assert.dom(settingActionButtonSelector);
+
+    settingActionButtons.exists(
+      { count: 1 },
+      'renders one setting action button'
+    );
+
+    settingActionButtons.hasText(
+      'Take action!',
+      'renders the correct setting action button content'
+    );
+    settingActionButtons.hasNoClass(
+      'Polaris-Button--primary',
+      'renders plain setting action button'
+    );
   });
 
-  assert.ok(
-    button.classList.contains('Polaris-Button--disabled'),
-    'button is in disabled state when disabled is true'
-  );
-  assert.notOk(
-    button.classList.contains('Polaris-Button--loading'),
-    'button is not in loading state when loading is false'
-  );
+  test('it handles the enabled attribute correctly', async function(assert) {
+    this.set('enabled', true);
+    await render(hbs`
+      {{polaris-setting-toggle
+        enabled=enabled
+        action=(hash
+          text="Flip the switch"
+        )
+      }}
+    `);
+
+    const settingActionButton = assert.dom(settingActionButtonSelector);
+    settingActionButton.exists('renders the setting action button');
+    settingActionButton.hasText(
+      'Flip the switch',
+      'renders the correct setting action button content'
+    );
+
+    settingActionButton.hasClass(
+      'Polaris-Button--primary',
+      'with enabled true - renders primary setting action button'
+    );
+
+    this.set('enabled', false);
+    settingActionButton.hasNoClass(
+      'Polaris-Button--primary',
+      'with enabled false - renders plain setting action button'
+    );
+  });
+
+  test('it handles the supplied action correctly', async function(assert) {
+    this.set('actionFired', false);
+    await render(hbs`
+      {{polaris-setting-toggle
+        enabled=enabled
+        action=(hash
+          text="Flip the switch"
+          onAction=(action (mut actionFired) true)
+        )
+      }}
+    `);
+
+    await click(settingActionButtonSelector);
+    assert.ok(this.get('actionFired'), 'fires the action when button clicked');
+  });
+
+  test("it obeys the passed-in action hash's loading and disabled flags", async function(assert) {
+    this.setProperties({
+      isLoading: true,
+      isDisabled: false,
+    });
+
+    await render(hbs`
+      {{polaris-setting-toggle
+        action=(hash
+          text="Toggle"
+          loading=isLoading
+          disabled=isDisabled
+          onAction=(action (mut dummy))
+        )
+      }}
+    `);
+
+    let button = assert.dom(settingActionButtonSelector);
+    button.hasClass(
+      'Polaris-Button--loading',
+      'button is in loading state when loading is true'
+    );
+
+    this.setProperties({
+      isLoading: false,
+      isDisabled: true,
+    });
+
+    button.hasClass(
+      'Polaris-Button--disabled',
+      'button is in disabled state when disabled is true'
+    );
+    button.hasNoClass(
+      'Polaris-Button--loading',
+      'button is not in loading state when loading is false'
+    );
+  });
 });
