d012d928398e99a95217fa9516537c1bb33613b2
vladucu <vanca.vlad@gmail.com>
2019-08-06 20:21:11 +0300

    Convert polaris-labelled to ES6 class

--- addon/components/polaris-labelled.js
+++ addon/components/polaris-labelled.js
@@ -1,21 +1,61 @@
 import Component from '@ember/component';
+import { tagName, layout } from '@ember-decorators/component';
 import { computed } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import {
   computedErrorId,
   computedHelpTextId,
 } from '@smile-io/ember-polaris/utils/id';
-import layout from '../templates/components/polaris-labelled';
+import template from '../templates/components/polaris-labelled';
 
 /**
  * Internal Polaris labelled component, used to add labels to form fields.
  */
-export default Component.extend({
-  // Tagless component so that Ember doesn't apply the `id`
-  // attribute to the component's root element.
-  tagName: '',
+@tagName('')
+@layout(template)
+export default class PolarisLabelledComponent extends Component {
+  /**
+   * Text for the label
+   *
+   * @type {String}
+   * @public
+   */
+  label = null;
 
-  layout,
+  /**
+   * Error to display beneath the label
+   *
+   * @type {String|Component|Boolean}
+   * @public
+   */
+  error = null;
+
+  /**
+   * An action
+   *
+   * @type {Object}
+   * @public
+   */
+  action = null;
+
+  /**
+   * Additional hint text to display
+   *
+   * @type {String|Component}
+   * @public
+   */
+  helpText = null;
+
+  /**
+   * Visually hide the label
+   *
+   * @type {Boolean}
+   * @default false
+   * @public
+   */
+  labelHidden = false;
+
+  dataTestLabelled = true;
 
   /**
    * A unique identifier for the label
@@ -25,52 +65,18 @@ export default Component.extend({
    * @type {String}
    * @public
    */
-  id: computed(function() {
+  @computed()
+  get id() {
+    if (this._id) {
+      return this._id;
+    }
+
     return guidFor(this);
-  }),
-
-  /**
-   * Text for the label
-   *
-   * @type {String}
-   * @public
-   */
-  label: null,
-
-  /**
-   * Error to display beneath the label
-   *
-   * @type {String|Component|Boolean}
-   * @public
-   */
-  error: null,
-
-  /**
-   * An action
-   *
-   * @type {Object}
-   * @public
-   */
-  action: null,
-
-  /**
-   * Additional hint text to display
-   *
-   * @type {String|Component}
-   * @public
-   */
-  helpText: null,
-
-  /**
-   * Visually hide the label
-   *
-   * @type {Boolean}
-   * @default false
-   * @public
-   */
-  labelHidden: false,
-
-  dataTestLabelled: true,
+  }
+  set id(value) {
+    this._id = value;
+    return value;
+  }
 
   /**
    * ID for the error message div
@@ -78,7 +84,8 @@ export default Component.extend({
    * @type {String}
    * @private
    */
-  errorId: computedErrorId('id').readOnly(),
+  @computedErrorId('id')
+  errorId;
 
   /**
    * ID for the help text div
@@ -86,7 +93,8 @@ export default Component.extend({
    * @type {String}
    * @private
    */
-  helpTextId: computedHelpTextId('id').readOnly(),
+  @computedHelpTextId('id')
+  helpTextId;
 
   /**
    * Flag indicating whether to render the error component
@@ -94,8 +102,9 @@ export default Component.extend({
    * @type {Boolean}
    * @private
    */
-  shouldRenderError: computed('error', function() {
+  @(computed('error').readOnly())
+  get shouldRenderError() {
     let error = this.get('error');
     return error && typeof error !== 'boolean';
-  }).readOnly(),
-});
+  }
+}
