c59e4785c2c0171823a530527284761e958e1407
vladucu <vanca.vlad@gmail.com>
2019-08-06 14:51:35 +0300

    Convert polaris-button to use ES6 classes

--- addon/components/polaris-button/content.js
+++ addon/components/polaris-button/content.js
@@ -1,26 +1,22 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import layout from '../../templates/components/polaris-button/content';
+import { classNames, tagName, layout } from '@ember-decorators/component';
+import template from '../../templates/components/polaris-button/content';
 
-/**
- * Internal component to keep rendering button content DRY.
- */
-export default Component.extend({
-  tagName: 'span',
-  classNames: ['Polaris-Button__Content'],
+@tagName('span')
+@classNames('Polaris-Button__Content')
+@layout(template)
+export default class ContentComponent extends Component {
+  text = null;
+  primary = false;
+  destructive = false;
+  loading = false;
+  disclosure = false;
+  icon = null;
 
-  layout,
-
-  // Properties passed down from parent polaris-button component.
-  text: null,
-  primary: false,
-  destructive: false,
-  loading: false,
-  disclosure: false,
-  icon: null,
-
-  spinnerColor: computed('primary', 'destructive', function() {
+  @(computed('primary', 'destructive').readOnly())
+  get spinnerColor() {
     let { primary, destructive } = this.getProperties('primary', 'destructive');
     return primary || destructive ? 'white' : 'inkLightest';
-  }).readOnly(),
-});
+  }
+}
