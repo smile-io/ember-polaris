0436e7245ddf0d39147dc41ecb50852268aa97c0
vladucu <vanca.vlad@gmail.com>
2019-11-28 15:40:09 +0200

    Convert polaris-badge component to ES6 class

--- addon/components/polaris-badge.js
+++ addon/components/polaris-badge.js
@@ -1,9 +1,15 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { isBlank, isPresent } from '@ember/utils';
 import { notEmpty } from '@ember/object/computed';
+import { isBlank, isPresent } from '@ember/utils';
 import { classify } from '@ember/string';
-import layout from '../templates/components/polaris-badge';
+import {
+  classNames,
+  classNameBindings,
+  tagName,
+  layout,
+} from '@ember-decorators/component';
+import template from '../templates/components/polaris-badge';
 
 const PROGRESS_LABELS = {
   incomplete: 'Incomplete',
@@ -30,13 +36,11 @@ const DEFAULT_SIZE = SIZES.medium;
  * Polaris badge component.
  * See https://polaris.shopify.com/components/images-and-icons/badge
  */
-export default Component.extend({
-  tagName: 'span',
-  classNames: ['Polaris-Badge'],
-  classNameBindings: ['statusClass', 'progressClass', 'sizeClass'],
-
-  layout,
-
+@tagName('span')
+@classNames('Polaris-Badge')
+@classNameBindings('statusClass', 'progressClass', 'sizeClass')
+@layout(template)
+export default class PolarisBadge extends Component {
   /**
    * The content to display inside the badge.
    *
@@ -44,110 +48,92 @@ export default Component.extend({
    * in which case the block content will be used
    * instead of `text`
    *
-   * @property text
    * @type {String}
    * @default: null
    * @public
    */
-  text: null,
+  text = null;
 
   /**
    * Set the color of the badge for the given status.
    *
-   * @property status
    * @type {String}
    * @default: null
    * @public
    */
-  status: null,
+  status = null;
 
   /**
    * Render a pip showing the progress of a given task.
    *
-   * @property progress
    * @type {String}
    * @default: null
    * @public
    */
-  progress: null,
+  progress = null;
 
   /**
    * Medium or small size. Use `small` only in the main navigation of an app frame..
    *
-   * @property size
    * @type {String}
    * @default: 'medium'
    * @public
    */
-  size: DEFAULT_SIZE,
+  size = DEFAULT_SIZE;
 
-  /**
-   * @private
-   */
-  hasProgress: notEmpty('progress'),
+  @notEmpty('progress')
+  hasProgress;
 
-  /**
-   * @private
-   */
-  hasStatus: notEmpty('status'),
+  @notEmpty('status')
+  hasStatus;
 
-  /**
-   * @private
-   */
-  progressDescription: computed('progress', function() {
-    const progress = this.get('progress');
+  @computed('progress')
+  get progressDescription() {
+    const { progress } = this;
     if (isBlank(progress) || progress === 'default') {
       return null;
     }
 
     return PROGRESS_LABELS[progress];
-  }).readOnly(),
+  }
 
-  /**
-   * @private
-   */
-  progressClass: computed('progress', function() {
-    const progress = this.get('progress');
+  @computed('progress')
+  get progressClass() {
+    const { progress } = this;
     if (isBlank(progress) || progress === 'default') {
       return null;
     }
 
     return `Polaris-Badge--progress${classify(progress)}`;
-  }).readOnly(),
+  }
 
-  /**
-   * @private
-   */
-  sizeClass: computed('size', function() {
-    const size = this.get('size');
+  @computed('size')
+  get sizeClass() {
+    const { size } = this;
     if (isPresent(size) && size !== DEFAULT_SIZE) {
       return `Polaris-Badge--size${classify(size)}`;
     }
 
     return null;
-  }).readOnly(),
+  }
 
-  /**
-   * @private
-   */
-  statusDescription: computed('status', function() {
-    const status = this.get('status');
+  @computed('status')
+  get statusDescription() {
+    const { status } = this;
     if (isBlank(status) || status === 'default') {
       return null;
     }
 
     return STATUS_LABELS[status];
-  }).readOnly(),
+  }
 
-  /**
-   * @private
-   */
-  statusClass: computed('status', function() {
-    const status = this.get('status');
+  @computed('status')
+  get statusClass() {
+    const { status } = this;
     if (isBlank(status) || status === 'default') {
       return null;
     }
 
     return `Polaris-Badge--status${classify(status)}`;
-  }).readOnly(),
-});
+  }
+}
