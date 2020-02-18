4c46d962e4e89a47c3b0684cabf4cd8e6477f7d8
Tom Netzband <tomnez@gmail.com>
2020-02-18 10:20:05 -0600

    Run codemods on popover components

--- addon/components/polaris-popover/content.js
+++ addon/components/polaris-popover/content.js
@@ -1,72 +1,71 @@
 import Component from '@ember/component';
+import { tagName, layout as templateLayout } from '@ember-decorators/component';
 import layout from '../../templates/components/polaris-popover/content';
 
-export default Component.extend({
-  tagName: '',
+@tagName('')
+@templateLayout(layout)
+export default class Content extends Component {
+ /**
+  * Automatically add wrap content in a section
+  *
+  * @property sectioned
+  * @type {Boolean}
+  * @default false
+  * @public
+  */
+ sectioned = false;
 
-  layout,
+ /**
+  * Allow popover to stretch to the full width of its activator
+  *
+  * @property fullWidth
+  * @type {Boolean}
+  * @default false
+  * @public
+  */
+ fullWidth = false;
 
-  /**
-   * Automatically add wrap content in a section
-   *
-   * @property sectioned
-   * @type {Boolean}
-   * @default false
-   * @public
-   */
-  sectioned: false,
+ /**
+  * Allow popover to stretch to fit content vertically
+  *
+  * @property fullHeight
+  * @type {Boolean}
+  * @default false
+  * @public
+  */
+ fullHeight = false;
 
-  /**
-   * Allow popover to stretch to the full width of its activator
-   *
-   * @property fullWidth
-   * @type {Boolean}
-   * @default false
-   * @public
-   */
-  fullWidth: false,
+ /**
+  * Content wrapper component.
+  *
+  * @property contentComponent
+  * @type {Component}
+  * @default: null
+  * @public
+  */
+ contentComponent = null;
 
-  /**
-   * Allow popover to stretch to fit content vertically
-   *
-   * @property fullHeight
-   * @type {Boolean}
-   * @default false
-   * @public
-   */
-  fullHeight: false,
+ /**
+  * Simple text for quick popovers.
+  *
+  * This component can be used in block form,
+  * in which case the block content will be used
+  * instead of `text`
+  *
+  * @property text
+  * @type {String}
+  * @default null
+  * @public
+  */
+ text = null;
 
-  /**
-   * Content wrapper component.
-   *
-   * @property contentComponent
-   * @type {Component}
-   * @default: null
-   * @public
-   */
-  contentComponent: null,
-
-  /**
-   * Simple text for quick popovers.
-   *
-   * This component can be used in block form,
-   * in which case the block content will be used
-   * instead of `text`
-   *
-   * @property text
-   * @type {String}
-   * @default null
-   * @public
-   */
-  text: null,
-
-  /**
-   * `ember-basic-dropdown`'s generated ID, used to look up
-   *
-   * @property uniqueId
-   * @type {String}
-   * @default: null
-   * @public
-   */
-  uniqueId: null,
-});
+ /**
+  * `ember-basic-dropdown`'s generated ID, used to look up
+  *
+  * @property uniqueId
+  * @type {String}
+  * @default: null
+  * @public
+  */
+ uniqueId = null;
+}
