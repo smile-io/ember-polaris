e1f49e019f21e61eada07efef92f578e20e3c419
vladucu <vanca.vlad@gmail.com>
2020-06-19 02:42:52 +0300

    Convert page-actions to ES6 class

--- addon/components/polaris-page-actions.js
+++ addon/components/polaris-page-actions.js
@@ -1,6 +1,10 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { action, computed } from '@ember/object';
 import { isArray } from '@ember/array';
+import {
+  classNames,
+  layout as templateLayout,
+} from '@ember-decorators/component';
 import { invokeAction } from 'ember-invoke-action';
 import layout from '../templates/components/polaris-page-actions';
 
@@ -8,41 +12,34 @@ import layout from '../templates/components/polaris-page-actions';
  * Polaris page actions component.
  * See https://polaris.shopify.com/components/structure/page-actions
  */
-export default Component.extend({
-  classNames: ['Polaris-PageActions'],
-
-  layout,
-
+@classNames('Polaris-PageActions')
+@templateLayout(layout)
+export default class PolarisPageActions extends Component {
   /**
    * The primary action for the page
    *
-   * @property primaryAction
    * @type {Object}
    * @default null
    * @public
    */
-  primaryAction: null,
+  primaryAction = null;
 
   /**
    * The secondary actions for the page
    *
-   * @property secondaryActions
    * @type {Array}
    * @default null
    * @public
    */
-  secondaryActions: null,
+  secondaryActions = null;
 
-  /**
-   * @private
-   */
-  showSecondaryActions: computed('secondaryActions', function() {
+  @(computed('secondaryActions').readOnly())
+  get showSecondaryActions() {
     return isArray(this.get('secondaryActions'));
-  }).readOnly(),
+  }
 
-  actions: {
-    fireAction(action) {
-      invokeAction(action, 'onAction');
-    },
-  },
-});
+  @action
+  fireAction(action) {
+    invokeAction(action, 'onAction');
+  }
+}
