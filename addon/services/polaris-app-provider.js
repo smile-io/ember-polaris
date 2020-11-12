fb8674da6cb42aa47707618f0bc24d21bc2603df
vladucu <vanca.vlad@gmail.com>
2020-11-12 17:11:07 +0200

    Cleanup

--- addon/services/polaris-app-provider.js
+++ addon/services/polaris-app-provider.js
@@ -5,11 +5,9 @@ export default class PolarisAppProviderService extends Service {
   @tracked _features = { newDesignLanguage: false };
 
   get features() {
-    console.log(this._features, 'get features');
     return this._features;
   }
   set features(features = {}) {
-    console.log('setting features');
     this._features = { ...this._features, ...features };
   }
 }
