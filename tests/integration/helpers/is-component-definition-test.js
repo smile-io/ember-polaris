83a1e79ef663f593488518cc467e35221fd9d0da
Andy Pye <apye@sweettoothhq.com>
2021-01-12 12:06:00 +0200

    Add failing test

--- tests/integration/helpers/is-component-definition-test.js
+++ tests/integration/helpers/is-component-definition-test.js
@@ -3,23 +3,49 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 
+function createMockGlimmerComponentDefinition() {
+  return {
+    [Symbol('INNER')]: {
+      name: 'mock-component',
+      ComponentClass: {},
+      template: {},
+      manager: {},
+      state: {},
+    },
+  };
+}
+
 module('Integration | Helper | is-component-definition', function (hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function (assert) {
-    this.set('inputValue', '1234');
-
-    await render(hbs`{{if (is-component-definition inputValue) "yes" "no"}}`);
+  test('it detects component definitions correctly', async function (assert) {
+    await render(hbs`{{if (is-component-definition "1234") "yes" "no"}}`);
     assert
       .dom(this.element)
-      .hasText('no', "returns false when it's not a component definition");
+      .hasText(
+        'no',
+        'returns false when passed something other than a component definition'
+      );
 
     await render(
       hbs`{{if (is-component-definition (component "polaris-heading" text="Text")) "yes" "no"}}`
     );
     assert
       .dom(this.element)
-      .hasText('yes', "returns true when it's a component definition");
+      .hasText('yes', 'returns true when passed a component definition');
+
+    this.set(
+      'glimmerComponentDefinition',
+      createMockGlimmerComponentDefinition()
+    );
+    await render(
+      hbs`{{if (is-component-definition this.glimmerComponentDefinition) "yes" "no"}}`
+    );
+    assert
+      .dom(this.element)
+      .hasText(
+        'yes',
+        'returns true when passed a Glimmer component definition'
+      );
   });
 });
