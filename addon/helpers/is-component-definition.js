9e24232e8bf91aad948e86b2a0a000d1b92cb011
Tom Netzband <tomnez@gmail.com>
2021-03-03 11:41:23 -0600

    Update is-component-definition for ember update

--- addon/helpers/is-component-definition.js
+++ addon/helpers/is-component-definition.js
@@ -12,9 +12,15 @@ export function isComponentDefinition(content) {
   if (symbolPropKeys?.length) {
     let isGlimmerComponentDefinition = symbolPropKeys.some((symbolPropKey) => {
       let propValue = content[symbolPropKey];
+      // The check for `__container__` is due to an update in Ember 3.25.1 where ComponentClass
+      // is no longer a key in the INNER symbol. As far as I can tell the combination of Symbols
+      // and having a `__container__` key is specific only to components. Passing other objects
+      // such as JS objects, Ember objects, hashes, or services, etc, do not share these traits.
       return (
         propValue &&
-        Object.keys(propValue).some((key) => key === 'ComponentClass')
+        Object.keys(propValue).some(
+          (key) => key === 'ComponentClass' || key === '__container__'
+        )
       );
     });
 
