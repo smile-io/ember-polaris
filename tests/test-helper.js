ccd3bbf09746ee22dd3cbe5deaff3265700dcfe9
vladucu <vanca.vlad@gmail.com>
2021-04-12 14:36:18 +0300

    Update with latest ember-cli blueprint
    Results for running `ember-cli-update` from `v3.23.0`...`v3.25.3`

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,8 @@
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
@@ -8,5 +10,7 @@ import { setup } from 'qunit-dom';
 setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
