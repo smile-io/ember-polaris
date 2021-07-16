9c1b2c939ad282827a3d9a634f884dbb91a16d8c
vladucu <vanca.vlad@gmail.com>
2021-07-16 11:09:13 +0300

    Use max-width of 100 for handlebars

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,5 +2,7 @@
 
 module.exports = {
   ...require('@smile-io/ember-styleguide/.prettierrc'),
-  overrides: [{ files: '*.hbs', options: { singleQuote: false } }],
+  overrides: [
+    { files: '*.hbs', options: { singleQuote: false, printWidth: 100 } },
+  ],
 };
