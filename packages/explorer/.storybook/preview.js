825f0936568608f11cbc467bed602842a823aa46
Vlad Vanca <vanca.vlad@gmail.com>
2023-09-05 23:58:09 +0300

    Better match polaris-react storybook structure

--- packages/explorer/.storybook/preview.js
+++ packages/explorer/.storybook/preview.js
@@ -1,3 +1,16 @@
+// TODO add other decorators, etc. from polaris-react storybook config
+import { breakpoints } from '@shopify/polaris-tokens';
+
+const viewPorts = Object.entries({
+  ...breakpoints,
+  'breakpoints-xs': '20rem', // Replace the 0px xs breakpoint with 320px (20rem) for testing small screens
+}).map(([key, value]) => {
+  return {
+    name: key,
+    styles: { width: value, height: '100%' },
+  };
+});
+
 export const parameters = {
   controls: {
     matchers: {
@@ -7,4 +20,5 @@ export const parameters = {
   },
   controls: { expanded: true },
   actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: { viewports: { ...viewPorts } },
 };
